4f52a3b3bc31857b603015fa056cf18e3695256b
TeRiRi-Superb <2308419543@qq.com>
2021-09-23 11:52:48 +0800

    前端订单

--- myobject/web/urls.py
+++ myobject/web/urls.py
@@ -1,21 +1,10 @@
-"""myobject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path
+#前台大堂点餐端子路由文件
+from django.urls import path,include
+from web.views.index import IndexView, LoginView, logout, verify
 
 urlpatterns = [
-
+    path('', IndexView.as_view(), name="web_index"),
+    path('login/', LoginView.as_view(), name="web_login"),
+    path('logout/', logout, name='web_logout'),
+    path('verify/', verify, name='web_verify'),
 ]
