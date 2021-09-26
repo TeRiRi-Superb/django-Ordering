165abcb63c45339486aabf0891c606089703adb1
TeRiRi-Superb <2308419543@qq.com>
2021-09-26 16:49:44 +0800

    子路由

--- myobject/web/urls.py
+++ myobject/web/urls.py
@@ -1,10 +1,18 @@
 #前台大堂点餐端子路由文件
-from django.urls import path,include
+from django.urls import path, include
 from web.views.index import IndexView, LoginView, logout, verify
+from web.views.cart import addcart, changecart, clearcart, delcart
 
 urlpatterns = [
-    path('web/', IndexView.as_view(), name="web_index"),
     path('login/', LoginView.as_view(), name="web_login"),
     path('logout/', logout, name='web_logout'),
     path('verify/', verify, name='web_verify'),
+
+    path('web/', include([
+        path('', IndexView.as_view(), name="web_index"),
+        path('cart/add/<str:pid>', addcart, name='addcart'),
+        path('cart/change/', changecart, name='changecart'),
+        path('cart/clear/', clearcart, name='clearcart'),
+        path('cart/del/<str:pid>', delcart, name='delcart'),
+    ])),
 ]
