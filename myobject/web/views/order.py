10da9c3db85f76da0aca49ca68fdd2b3ad1113dd
TeRiRi-Superb <2308419543@qq.com>
2021-09-28 09:36:23 +0800

    订单

---
+++ myobject/web/views/order.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.views.generic import View
+
+
+class ShowOrder(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+def DelOrder(request):
+    pass
+
+
+def DetailOrder(request):
+    pass
