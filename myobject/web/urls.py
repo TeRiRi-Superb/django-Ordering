bfb800c3873e1bdaf42472194e591eabfab20d46
TeRiRi-Superb <2308419543@qq.com>
2021-09-30 11:47:34 +0800

    子路由

--- myobject/web/urls.py
+++ myobject/web/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, include
 from web.views.index import IndexView, LoginView, logout, verify
 from web.views.cart import addcart, changecart, clearcart, delcart
+from web.views.order import ShowOrder, DetailOrder, DelOrder
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name="web_login"),
@@ -14,5 +15,8 @@ urlpatterns = [
         path('cart/change/<str:pid>', changecart, name='changecart'),
         path('cart/clear/', clearcart, name='clearcart'),
         path('cart/del/<str:pid>', delcart, name='delcart'),
+        path('order/show<str:pIndex>', ShowOrder.as_view(), name='showorder'),
+        path('order/Del', DelOrder, name='delorder'),
+        path('order/Detail', DetailOrder, name='detailorder'),
     ])),
 ]
